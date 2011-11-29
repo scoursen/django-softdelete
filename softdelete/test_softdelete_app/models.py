8466e70981ba60ff5bb115d3eec828c6e585ee98
Stephen Coursen <smcoursen@gmail.com>
2011-11-29 16:40:44 -0700

    Removed unused __unicode__

--- softdelete/test_softdelete_app/models.py
+++ softdelete/test_softdelete_app/models.py
@@ -5,25 +5,13 @@ from softdelete.admin import *
 
 class TestModelOne(SoftDeleteObject):
     extra_bool = models.BooleanField(default=False)
-
-    def __unicode__(self):
-        return u"TestModelOne - extra_bool: %s, deleted: %s" % (self.extra_bool,
-                                                                self.deleted)
     
 class TestModelTwo(SoftDeleteObject):
     extra_int = models.IntegerField()
     tmo = models.ForeignKey(TestModelOne,related_name='tmos')
     
-    def __unicode__(self):
-        return u"TestModelTwo - extra_int: %s, tmo: %s, deleted: %s" % (self.extra_int,
-                                                           self.tmo,
-                                                           self.deleted)
-
 class TestModelThree(SoftDeleteObject):
     tmos = models.ManyToManyField(TestModelOne, through='TestModelThrough')
-
-    def __unicode__(self):
-        return u"TestModelThree -- %d" % self.pk
 
 class TestModelThrough(SoftDeleteObject):
     tmo1 = models.ForeignKey(TestModelOne, related_name="left_side")
