f8764453d1e9f9b3f4be279eacb7849d0eb31540
Rolf Erik Lekang <relekang@gmail.com>
2013-02-25 22:11:05 +0100

    fix append problem in tests

--- softdelete/tests/__init__.py
+++ softdelete/tests/__init__.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from softdelete.tests.test_sd import *
 from softdelete.tests.test_views import *
 
-settings.INSTALLED_APPS.append('softdelete.test_softdelete_app')
+settings.INSTALLED_APPS += ('softdelete.test_softdelete_app',)
 loading.cache.loaded = False
 call_command('syncdb', interactive=False, verbosity=False)
 
