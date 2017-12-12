7536ddac29da58b455e79e9baa6ee03f134db459
Mikkel Munch Mortensen <3xm@detfalskested.dk>
2017-12-12 11:16:42 +0100

    Include `on_delete` in migration
    I'm intentionally adding this to the existing migration.
    
    `makemigrations` do not create a new migration for the `on_delete`
    changes to the fields.
    
    I also tried adding a new model without the argument (and Django 1.11),
    for which the migration _did_ include the argument (defaults to
    `models.CASCADE`). If I explicitly removed the `on_delete` argument from
    that migration, added `on_delete` to the model and ran `makemigrations`
    again, it did not create a new migration.
    
    So I assume it's safe to just add it to the existing migration, to make
    it compatible with the most recent versions of Django.

--- softdelete/migrations/0001_initial.py
+++ softdelete/migrations/0001_initial.py
@@ -18,7 +18,7 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('created_date', models.DateTimeField(default=django.utils.timezone.now)),
                 ('object_id', models.CharField(max_length=100)),
-                ('content_type', models.ForeignKey(to='contenttypes.ContentType')),
+                ('content_type', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='contenttypes.ContentType')),
             ],
         ),
         migrations.CreateModel(
@@ -27,8 +27,8 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('created_date', models.DateTimeField(default=django.utils.timezone.now)),
                 ('object_id', models.CharField(max_length=100)),
-                ('changeset', models.ForeignKey(related_name='soft_delete_records', to='softdelete.ChangeSet')),
-                ('content_type', models.ForeignKey(to='contenttypes.ContentType')),
+                ('changeset', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='soft_delete_records', to='softdelete.ChangeSet')),
+                ('content_type', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='contenttypes.ContentType')),
             ],
         ),
         migrations.AlterUniqueTogether(
